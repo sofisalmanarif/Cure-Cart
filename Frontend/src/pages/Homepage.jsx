cfb000a3179cb9117c1cb8c12d5547083b29b814
sofisalmanarif <sofisalman9906@gmail.com>
2024-08-30 22:44:17 +0530

    lazy loading in home page

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -1,23 +1,26 @@
-import Herosection from "../Components/Herosection";
-import Deals from "../Components/Deals";
-import Prescription from "../Components/Prescription";
-import BabyFood from "../Components/BabyFood";
-import Extras from "../Components/Extras";
-import Ccfooter from "../Components/footer/Ccfooter";
+import React, { Suspense } from 'react';
 
-
+// Lazy load components
+const Herosection = React.lazy(() => import("../Components/Herosection"));
+const Deals = React.lazy(() => import("../Components/Deals"));
+const Prescription = React.lazy(() => import("../Components/Prescription"));
+const BabyFood = React.lazy(() => import("../Components/BabyFood"));
+const Extras = React.lazy(() => import("../Components/Extras"));
+const Ccfooter = React.lazy(() => import("../Components/footer/Ccfooter"));
 
 
 function Homepage() {
   return (
-    <div>
+    <div >
+      <suspense fallback={<div>Loading...</div>}
+      >
       <Herosection />
       <Deals />
       <Prescription />
       <BabyFood />
       <Extras />
       <Ccfooter/>
-      
+      </suspense>
     </div>
   );
 }
